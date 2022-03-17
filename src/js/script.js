80622673084c13615a98dbd76730600ba549d744
Erik <ohanesian.erik@gmail.com>
2022-03-17 20:59:05 +0200

    carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,16 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+$(document).ready(function () {
+	$('.carousel__inner').slick({
+		speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left-arrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right-arrow.png"></button>',
+		responsive: [
+			{
+			  breakpoint: 768,
+			  settings: {
+				  dots: true,
+				  arrows: false
+			  }
+			}
+		  ]
+	});
+});
