613e80c5c97e0abb7c1255a196d5b6d9fbaa5f46
Erik <ohanesian.erik@gmail.com>
2022-03-18 20:54:36 +0200

    fixed tabs

--- src/js/script.js
+++ src/js/script.js
@@ -24,8 +24,8 @@ $(document).ready(function () {
 		$(item).each(function(i) {
 			$(this).on('click', function(e) {
 				e.preventDefault();
-				$('.catalog-item__content').eq(i).toggleClass('.catalog-item__content_active');
-				$('.catalog-item__list').eq(i).toggleClass('.catalog-item__list_active');
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
 			})
 		});
 	};
