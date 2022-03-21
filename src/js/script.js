37d77769745fd1c9a4027066555980e5697b51cb
Erik <ohanesian.erik@gmail.com>
2022-03-21 16:42:52 +0200

    add pageup

--- src/js/script.js
+++ src/js/script.js
@@ -99,9 +99,39 @@ $(document).ready(function () {
 			$(this).find("input").val("");
 			$('#consultation, #order').fadeOut();
 			$('.overlay, #thanks').fadeIn();
-
 			$('form').trigger('reset');
 		});
 		return false;
 	});
+
+	// Smooth scroll and pageup
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+	$("a").on('click', function(event) {
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+		  // Prevent default anchor click behavior
+		  event.preventDefault();
+	
+		  // Store hash
+		  var hash = this.hash;
+	
+		  // Using jQuery's animate() method to add smooth page scroll
+		  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+		  $('html, body').animate({
+			scrollTop: $(hash).offset().top
+		  }, 800, function(){
+	
+			// Add hash (#) to URL when done scrolling (default click behavior)
+			window.location.hash = hash;
+		  });
+		} // End if
+	  });
 });
