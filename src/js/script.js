95b64ff3ad6bb4b3fd8eebbd1782a78bd27397e4
Erik <ohanesian.erik@gmail.com>
2022-03-22 13:50:10 +0200

    validate

--- src/js/script.js
+++ src/js/script.js
@@ -114,7 +114,7 @@ $(document).ready(function () {
 		}
 	});
 
-	$("a").on('click', function(event) {
+	$("a[href=#up]").on('click', function(event) {
 		// Make sure this.hash has a value before overriding default behavior
 		if (this.hash !== "") {
 		  // Prevent default anchor click behavior
